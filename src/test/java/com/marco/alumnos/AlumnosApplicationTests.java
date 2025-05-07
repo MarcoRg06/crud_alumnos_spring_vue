bbc2ef778e9d039060d742ddb0890ad2dbb72df4
MarcoRg06 <22620119@tlaxiaco.tecnm.mx>
2025-05-07 15:24:11 -0600

    Subiendo el proyecto al repositorio

---
+++ src/test/java/com/marco/alumnos/AlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
