f7a51560c8067ed0d7ffe92a0c3a4593a0a282ce
MarcoRg06 <22620119@tlaxiaco.tecnm.mx>
2025-05-08 22:57:06 -0600

    Act6-Creando Metodo PUT en el controlador

--- src/main/java/com/marco/alumnos/controller/AlumnoController.java
+++ src/main/java/com/marco/alumnos/controller/AlumnoController.java
@@ -1,7 +1,9 @@
 package com.marco.alumnos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
 import com.marco.alumnos.model.Alumno;
 import com.marco.alumnos.repository.AlumnoRepository;
 
@@ -27,5 +31,20 @@ public class AlumnoController {
     @PostMapping("/insertar-alumnos")
     public Alumno InsertarAlumno(@RequestBody Alumno alumno) {
         return alumnoRepository.save(alumno);
+    }
+    //Metodo para eidtar un alumno de la base de datos
+    @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<Alumno> EditarAlumno(@PathVariable Long id,@RequestBody Alumno alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellidos(alumno.getApellidos());
+            alumnoExistente.setEmail(alumno.getEmail());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setImagenURL(alumno.getImagenURL());
+            Alumno actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
