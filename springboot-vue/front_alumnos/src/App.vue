d1a434e558d494ff4a38085566296a7f0e8ff3e0
Diegocruz040708 <cruzdiego040708@gmail.com>
2025-05-13 02:35:50 -0600

    Act13-Creando Formularios e instalando la libreria de los iconos de Bootstrap

--- springboot-vue/front_alumnos/src/App.vue
+++ springboot-vue/front_alumnos/src/App.vue
@@ -3,6 +3,13 @@ import { ref, onMounted} from 'vue';
 import axios from 'axios';
 
 const alumnos = ref([]); // Definimos una variable reactiva para almacenar los alumnos
+const nuevoAlumno = ref({
+    nombre: '',
+    apellidos: '',
+    carrera: '',
+    telefono: '',
+    imagenURL: ''
+});
 const cargarAlumnos = async () => {
     const response = await axios.get('http://localhost:8081/alumnos/traer-lumnos');//traer todos los alumnos
     alumnos.value = response.data;
@@ -13,7 +20,70 @@ onMounted(cargarAlumnos); // Llamamos a la función cargarAlumnos cuando el comp
 
 <template>
 <div class="container">
-  <button class="btn btn-primary">boton</button>
+  <div class="row">
+    <div class="col-md-12 mt-4">
+      <div class="card shadow p-4 mb-4">
+        <h2 class="text-center">Formulario de Alumnos</h2>
+        <form @submit.prevent="agregarAlumno">
+          <div class="row">
+          <div class="col-md-6 mb-3">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre" v-model="nuevoAlumno.nombre" required>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="apellidos" class="form-label">Apellidos</label>
+            <input type="text" class="form-control" id="apellidos" v-model="nuevoAlumno.apellidos" required>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="carrera" class="form-label">Carrera</label>
+            <input type="text" class="form-control" id="carrera" v-model="nuevoAlumno.carrera" required>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="telefono" class="form-label">Telefono</label>
+            <input type="text" class="form-control" id="telefono" v-model="nuevoAlumno.telefono" required>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="imagenURL" class="form-label">Imagen URL</label>
+            <input type="text" class="form-control" id="imagenURL" v-model="nuevoAlumno.imagenURL">
+          </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Agregar Alumno</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="col-md-12">
+      <h2>Tabla de Alumnos</h2>
+  
+          <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Carrera</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Imagen</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="alumno in alumnos" :key="alumno.id">
+      <td>{{ alumno.id }}</td>
+      <td>{{ alumno.nombre }}</td>
+      <td>{{ alumno.apellidos }}</td>
+      <td>{{ alumno.carrera }}</td>
+      <td>{{ alumno.telefono }}</td>
+      <td><img :src="alumno.imagenURL" alt="Imagen de Alumno" width="50" height="50"></td>
+      <td>
+        <button class="btn btn-danger">Eliminar</button>
+      </td>
+    </tr>    
+  </tbody>
+</table>
+    </div>
+  </div>
+  
 </div>
 </template>
 
